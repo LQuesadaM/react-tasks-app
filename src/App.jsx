3657298759c350af330c2253b3a14128c7619870
LQuesadaM <luisquesada163@gmail.com>
2024-05-25 15:55:15 -0300

    🍻 componentes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,31 @@
-import { useState, useEffect } from "react";
-import "./App.css";
+import { TaskList } from './TaskList'
+import { TaskForm } from './TaskForm'
+import { tasks as data } from './tasks'
+import { useState, useEffect } from 'react'
+import './App.css'
 
 export const App = () => {
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    setTasks(data)
+  }, [])
+
+  const createTask = (taskTitle) => {
+    setTasks([
+      ...tasks,
+      {
+        title: taskTitle,
+        id: tasks.length,
+        description: 'mueva tarea',
+      },
+    ])
+  }
+
   return (
-    <div>
-      <h1>Hola Mundo</h1>
-    </div>
-  );
-};
+    <>
+      <TaskForm createTask={createTask} />
+      <TaskList tasks={tasks} />
+    </>
+  )
+}
---
+++ src/tasks.js
@@ -0,0 +1,17 @@
+export const tasks = [
+  {
+    id: 0,
+    title: 'Mi primer tarea',
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.'
+  },
+  {
+    id: 1,
+    title: 'Mi segunda tarea',
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.'
+  },
+  {
+    id: 2,
+    title: 'Mi tercer tarea',
+    description: 'Lorem ipsum dolor sit amet consectetur adipiscing elit.'
+  },
+]
