594c389e0f8338e250e7efaf60a155b9e8c89f4c
LQuesadaM <luisquesada163@gmail.com>
2024-05-27 16:21:00 -0300

    💄 style UI

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,11 @@ import { TaskForm } from './components/TaskForm'
 
 export const App = () => {
   return (
-    <>
-      <TaskForm />
-      <TaskList />
-    </>
+    <main className="bg-zinc-900 h-screen">
+      <div className="container mx-auto p-10">
+        <TaskForm />
+        <TaskList />
+      </div>
+    </main>
   )
 }
--- src/components/TaskCard.jsx
+++ src/components/TaskCard.jsx
@@ -3,10 +3,15 @@ import { TaskContext } from '../context/Context.jsx'
 
 export const TaskCard = ({ task, deleteTask }) => {
   return (
-    <div>
-      <h1>{task.title}</h1>
-      <p>{task.description}</p>
-      <button onClick={() => deleteTask(task.id)}>Eliminar Tarea</button>
+    <div className="bg-gray-800 text-white p-4 rounded-md">
+      <h1 className="text-xl font-bold capitalize">{task.title}</h1>
+      <p className="text-gray-500 text-sm">{task.description}</p>
+      <button
+        className="bg-blue-500 px-2 py-1 mt-4 rounded-md hover:bg-blue-600 active:bg-blue-700"
+        onClick={() => deleteTask(task.id)}
+      >
+        Eliminar Tarea
+      </button>
     </div>
   )
 }
